54a6c6caa4907aeec2946380285b740c834159a2
jmekavibul <jm9hp@virginia.edu>
2018-01-03 13:46:52 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# API Practice
+
+You better believe that we are making the best Star Wars implementation of the SWAPI that the galaxy has ever seen!
---
+++ index.js
@@ -0,0 +1,12 @@
+window.onload(function() {
+
+})
+
+setTimeout(function() {
+  let container = document.getElementById("greeting");
+  container.innerHTML = "<h1>Greetings from the Dark Side!</h1>";
+}, 5000);
+
+//generate two random numbers from range 0 to 37
+//Print the object from a list of objects (containing ships)
+//compare values within that objects e.g. cost or speed
